e71c73442ec043de18438d234d6e984aa7976992
Grigoriy <egrigoriy@gmail.com>
2016-12-14 17:54:48 +0300

    Aged Brie item refactored to method: aged_brie

--- src/gilded_rose.php
+++ src/gilded_rose.php
@@ -11,6 +11,7 @@ class GildedRose {
     function normal ($item) {
     
     	if ($item->quality == 0) {
+    		$item->sell_in = $item->sell_in - 1;
     		return;
     	}
     	if ($item->sell_in <= 0)
@@ -20,11 +21,29 @@ class GildedRose {
     	$item->sell_in = $item->sell_in - 1;    			 
     }
     
+    function aged_brie ($item) {
+    	if ($item->quality >= 50) {
+    		$item->sell_in = $item->sell_in - 1;
+    		return;
+    	}
+    	if ($item->sell_in <= 0) 
+    			$item->quality = $item->quality + 2;
+    	else
+    			$item->quality = $item->quality + 1;    			 
+    	$item->sell_in = $item->sell_in - 1;
+    
+    }
+    
     function update_quality() {
         foreach ($this->items as $item) {
         	
         	if ($item->name == "Normal") {
         		$this->normal($item);
+        		continue;
+        	}
+        	
+        	if ($item->name == "Aged Brie") {
+        		$this->aged_brie($item);
         		continue;
         	}
         	
