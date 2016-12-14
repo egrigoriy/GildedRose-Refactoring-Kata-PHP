a747b4aafaf53abc0b5b0a2f7b2a36d1fe3b0434
Grigoriy <egrigoriy@gmail.com>
2016-12-14 17:34:54 +0300

    Normal item refactored to method: normal

--- src/gilded_rose.php
+++ src/gilded_rose.php
@@ -8,8 +8,26 @@ class GildedRose {
         $this->items = $items;
     }
 
+    function normal ($item) {
+    
+    	if ($item->quality == 0) {
+    		return;
+    	}
+    	if ($item->sell_in <= 0)
+    		$item->quality = $item->quality - 2;
+    	else
+    		$item->quality = $item->quality - 1;
+    	$item->sell_in = $item->sell_in - 1;    			 
+    }
+    
     function update_quality() {
         foreach ($this->items as $item) {
+        	
+        	if ($item->name == "Normal") {
+        		$this->normal($item);
+        		continue;
+        	}
+        	
             if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($item->quality > 0) {
                     if ($item->name != 'Sulfuras, Hand of Ragnaros') {
