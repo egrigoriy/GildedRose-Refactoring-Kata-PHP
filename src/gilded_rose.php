ca7747fd8c4a8f74c57f9679d0d74eaf923d4449
Grigoriy <egrigoriy@gmail.com>
2016-12-15 15:42:52 +0300

    update_quality method was refactored to single switch statement(initial body code was deleted)

--- src/gilded_rose.php
+++ src/gilded_rose.php
@@ -53,71 +53,20 @@ class GildedRose {
     function update_quality() {
         foreach ($this->items as $item) {
         	
-        	if ($item->name == "Normal") {
-        		$this->normal_tick($item);
-        		continue;
-        	}
-        	
-        	if ($item->name == "Aged Brie") {
-        		$this->aged_brie_tick($item);
-        		continue;
-        	}
-        	
-        	if ($item->name == "Sulfuras, Hand of Ragnarose") {
-        		$this->sulfuras_tick($item);
-        		continue;
-        	}
-        	
-        	if ($item->name == "Backstage passes to a TAFKAL80ETC concert") {
-        		$this->backstage_tick($item);
-        		continue;
-        	}
-        	
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-            
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $item->sell_in - 1;
-            }
-            
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
-            }
+        	switch ($item->name) {
+        		case "Normal":
+        			$this->normal_tick($item);
+        		break;
+        		case "Aged Brie":
+        			$this->aged_brie_tick($item);
+        		break;
+        		case "Sulfuras, Hand of Ragnarose":
+        			$this->sulfuras_tick($item);
+        		break;
+        		case "Backstage passes to a TAFKAL80ETC concert":
+        			$this->backstage_tick($item);
+        		break;
+        	}  	
         }
     }
 }
